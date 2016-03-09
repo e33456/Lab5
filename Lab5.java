6683b2e26f6742c5f31a955488fce87de3719a3d
e33456 <erikalynnroberts@gmail.com>
2016-03-09 12:02:08 -0500

    stuff

---
+++ Lab5.java
@@ -0,0 +1,34 @@
+public class Lab5
+{
+public static void displayDataTypes()
+{
+  String str = "Hello, World!";
+  double d = 8.9;
+  int i = 4;
+  boolean b = true;
+  char c = 'b';
+  System.out.println("String: " + str);
+  System.out.println("double: " + d);
+  System.out.println("int: " + i);
+  System.out.println("boolean: " + b);
+  System.out.println("char: " + c);
+}
+
+public static void displayArtwork()
+{
+System.out.println("\n          ****    ****\n        *******  *******\n       ******************\n        ****************\n         *************\n \t *********\n   \t  *******\n     \t    ***");
+}
+
+public static double circleArea(int r)
+{
+  
+}
+
+public static double linearDistance(int x1, int y1, int x2, int y2)
+  
+{
+  
+}
+
+
+}
